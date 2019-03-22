446cdf963d3b7d433aaebbe138c5b6bb7c33a6d3
Animesh Kansal <animesh.kansal@utdallas.edu>
2019-03-21 21:38:09 -0500

    Update Exercise.sql

--- SQL Movie-Rating Query Exercises/Exercise.sql
+++ SQL Movie-Rating Query Exercises/Exercise.sql
@@ -26,7 +26,8 @@ from Movie
 where mID not in ( select mID from Rating )
 
 Q4
-Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date. 
+Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings
+with a NULL value for the date. 
 
 A4
 select name 
@@ -37,7 +38,8 @@ where ratingDate is null
 
 
 Q5
-Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. 
+Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and 
+ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. 
 
 A5
 select rv.name,m.title,rt.stars,rt.ratingDate 
@@ -48,7 +50,8 @@ order by rv.name,m.title,rt.stars
 
 
 Q6
-For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. 
+For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time,
+return the reviewer's name and the title of the movie. 
 
 A6
 select rv.name,m.title
@@ -59,7 +62,8 @@ where T.mID = m.mID and T.rID = rv.rID
 
 
 Q7
-For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie title and number of stars. Sort by movie title. 
+For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie 
+title and number of stars. Sort by movie title. 
 
 
 A7
@@ -72,7 +76,8 @@ order by title
 
 
 Q8
-For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest ratings given to that movie. Sort by rating spread from highest to lowest, then by movie title. 
+For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest 
+ratings given to that movie. Sort by rating spread from highest to lowest, then by movie title. 
 
 A8
 select title, max(stars) - min(stars) as ratingSpread
@@ -84,8 +89,11 @@ order by ratingspread desc,title
 
 
 Q9
-Find the difference between the average rating of movies released before 1980 and the average rating of movies released after 1980. (Make sure to calculate the average rating for each movie, then the average of those averages for movies before 1980 and movies after. Don't just calculate the overall average rating before and after 1980.) 
+Find the difference between the average rating of movies released before 1980 and the average rating of movies
+released after 1980. (Make sure to calculate the average rating for each movie, then the average of those 
+averages for movies before 1980 and movies after. Don't just calculate the overall average rating before and after 1980.) 
 
+A9
 select (select avg(stars)
 from (select m.mID,avg(stars) as stars ,year
 from Rating rt, movie m
